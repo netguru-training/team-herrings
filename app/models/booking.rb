3a67a5f0856a9f26dba8f779c4a6b4af569fe850
Wojciech Stępniak <wojtek@niom.pl>
2015-12-12 11:10:37 +0100

    #6 Add booking model

---
+++ app/models/booking.rb
@@ -0,0 +1,5 @@
+class Booking < ActiveRecord::Base
+  validates :date, :status, :customer_id, :table_id, presence: true
+
+  enum status: [ :pending, :rejected, :accepted]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212092448) do
+ActiveRecord::Schema.define(version: 20151212094844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "date"
+    t.integer  "status"
+    t.string   "reject_reason"
+    t.integer  "customer_id"
+    t.integer  "table_id"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.string   "name"

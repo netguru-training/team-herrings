1c04fb4a9625eb7b86a38ff775dcef2c9eeb4530
olhor <slawomir.kluczyk@gmail.com>
2015-12-12 15:48:53 +0100

    Merge branch 'master' of /home/suaveck/projects/team-herrings with conflicts.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,17 @@
 class AdminController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_admin!
 
   def dashboard
 
+  end
+
+  private
+
+  def authenticate_admin!
+    user = authenticate_user!
+    unless user.is_admin?
+      flash[:error] = 'Allowed only for admins'
+      redirect_to root_path
+    end
   end
 end

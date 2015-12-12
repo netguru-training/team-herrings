29701a6bd7dd9da11e30502f161288d0aececa35
olhor <slawomir.kluczyk@gmail.com>
2015-12-12 15:47:56 +0100

    Added administration stub (controller, etc.)
     - Added dashboard view stub
     - Added users stub
    
    Admin namespace requires auth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    role == ADMIN_ROLE
+  end
 end

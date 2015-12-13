dd4197e3144f88ef4620cf88ab734dd295e4dfa4
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2015-12-13 20:13:22 +0100

    [#11] Move BookingsController to Admin namespace, change links and redirects to work with it

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,4 +1,4 @@
-class BookingsController < ApplicationController
+class Admin::BookingsController < AdminController
   before_action :authenticate_user!, only: [:show, :edit, :index, :accept, :reject]
 
   expose_decorated(:bookings) { fetch_bookings }
@@ -23,7 +23,7 @@ class BookingsController < ApplicationController
 
   def update
     if booking.save
-      redirect_to booking_path(booking), notice: I18n.t('shared.updated', resource: 'Booking')
+      redirect_to admin_booking_path(booking), notice: I18n.t('shared.updated', resource: 'Booking')
     else
       render :edit
     end
@@ -34,18 +34,19 @@ class BookingsController < ApplicationController
 
     if booking.accepted!
       BookingMailer.booking_accepted_email(booking).deliver_now
-      redirect_to bookings_path, notice: I18n.t('booking.accepted', date: booking.date)
+      redirect_to admin_bookings_path, notice: I18n.t('booking.accepted', date: booking.date)
     else
       render :show
     end
   end
 
   def reject
-    if booking.rejected! && booking.update_attributes(booking_reject_params)
+    self.booking = Booking.find(params[:id])
+    if booking.update_attributes(booking_reject_params)
       BookingMailer.booking_rejected_email(booking).deliver_now
-      redirect_to bookings_path, notice: I18n.t('booking.rejected', date: booking.date)
+      redirect_to admin_bookings_path, notice: I18n.t('booking.rejected', date: booking.date)
     else
-      render :show
+      render :edit
     end
   end
 
@@ -54,23 +55,21 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:date, :status, :password, :password_confirmation, customer_attributes: [:first_name, :last_name, :email])
   end
-  
-  def user_redirector    
+
+  def user_redirector
     if current_user && current_user.admin?
-      redirect_to booking_path(booking), notice: I18n.t('shared.created', resource: 'Booking')
-    else      
-      redirect_to bookings_path, notice: I18n.t('booking.created')
-    end           
+      redirect_to admin_booking_path(booking), notice: I18n.t('shared.created', resource: 'Booking')
+    else
+      redirect_to admin_bookings_path, notice: I18n.t('booking.created')
+    end
   end
 
   def booking_reject_params
-    params.require(:booking).permit(:reject_reason)
+    params.require(:booking).permit(:reject_reason).merge(status: 'rejected')
   end
 
   def fetch_bookings
-#    if user_signed_in?
-      _bookings = current_user.admin? ? Booking : current_user.bookings
+      _bookings = current_user.admin? ? Booking.all : current_user.bookings
       params[:status] == 'pending' ? _bookings.pending.order('date desc') : _bookings.order('date asc')
-#    end
   end
 end

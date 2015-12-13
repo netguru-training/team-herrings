8c886dbe7e89db3d7794ee7deed4259761c2583d
Bartosz Nowak <sztosz@gmail.com>
2015-12-13 12:53:50 +0100

    #4 Added to_s to table model

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,4 +2,8 @@ class Table < ActiveRecord::Base
   validates :places, :location, presence: true
   enum location: [:indoor, :outdoor]
   validates :location, inclusion: { in: Table.locations.keys }
+
+  def to_s
+    "#{id} with #{places} places"
+  end
 end
